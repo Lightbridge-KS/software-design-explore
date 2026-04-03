cc3c32b22ad36db6a752b1ebb23c19574ee30e38
Lightbridge-KS <ki11ip0.s.a.s@gmail.com>
2026-04-03 20:54:20 +0700

    First commit

---
+++ CLAUDE.md
@@ -0,0 +1,3 @@
+# Software Design Exploration Book
+
+A personal book to explore software design with real-world examples. Use A Starlight Astro book. 
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Software Design Explore',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          sidebar: [
+              {
+                  label: 'OSS Examples',
+                  collapsed: false,
+                  items: [
+                      { label: 'MONAI Deploy Informatics Gateway', slug: 'oss/monai-deploy-info-gw-system' },
+                  ],
+              },
+              {
+                  label: 'Architecture Patterns',
+                  collapsed: false,
+                  items: [
+                      { label: 'Plugin Architecture: MONAI Deploy Info GW', slug: 'arch-patterns/monai-deploy-plugin-arch' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  collapsed: false,
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
+      mermaid({
+          theme: 'forest',
+          autoTheme: true
+      })
 	],
+
+  adapter: netlify(),
 });
